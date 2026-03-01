2ff6912d8f32ebd11d57a6f2f1eed6f59c9cc6eb
soikot <90247093+shaikat58-deb@users.noreply.github.com>
2026-03-01 11:49:51 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,50 @@
+const name = "soliaman";
+
+if (name.length) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}
+
+
+const name1 = " ";
+
+if (name1.length) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}
+
+
+const name3 = "0";
+
+if (name3.length) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}
+
+const name4 = null;
+
+if (name4) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}
+
+const name5 = NaN;
+
+if (name5) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}
+
+
+const name6 = {};
+
+if (name6) {
+    console.log("condition is true");
+} else {
+    console.log("condtion is false");
+}

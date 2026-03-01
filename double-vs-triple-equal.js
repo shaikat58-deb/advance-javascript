3d4264671b485c0abb6a072f815fe5dd91f150e7
soikot <90247093+shaikat58-deb@users.noreply.github.com>
2026-03-01 12:30:24 +0600

    double-vs-triple-equal.js

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,27 @@
+const first = 2;
+const second = "2";
+if (first == second) {
+    console.log("condtion is true");
+} else {
+  console.log("conditon is not trues");
+    
+}
+
+const first1 = 2;
+const second1 = "2";
+if (first1 === second1) {
+    console.log("condtion is true");
+} else {
+  console.log("conditon is not trues");
+   
+  
+}
+
+const first2 = 1;
+const second2 = true;
+if (first2 == second2) {
+    console.log("condtion is true");
+} else {
+  console.log("conditon is not trues");
+    
+}
